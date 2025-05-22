c35111220348fd322ac40296418a545913a66012
hasrat13 <hasrat_rahil@students.edu.sg>
2025-05-22 16:40:46 +0800

    Add files via upload

---
+++ print.py
@@ -0,0 +1,11 @@
+# printing a greeting
+print("hello , everyone")
+ 
+# printing my name 
+print("my name is Hasrat")
+
+# printing a fun fact about myself 
+print("i have been doing drama for 5 years ")
+
+# printing a simple calculation 
+print("print the sum of 10 + 5 is:" ,10+5)
