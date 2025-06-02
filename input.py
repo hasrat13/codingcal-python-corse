acef6bb57190d54331547c81a492422f1d4245d2
hasrat13 <hasrat_rahil@students.edu.sg>
2025-06-02 18:13:17 +0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,10 @@
+# input a word
+text=str(input("Enter a string : "))
+
+#reverse string
+# using step value -1 to iterate in reverse
+revText= text[::-1]
+text=revText
+
+print(" reverse of given string is:")
+print(text)
---
+++ values.py
@@ -0,0 +1,12 @@
+# lets check the datatypes of differnt values
+a=3
+print("type of a  " , type(a))
+
+b=14.5
+print("type of b ",type (b))
+
+c="drawing"
+print("type of c " , type(c))
+
+d=True
+print("type of d " , type(d))
