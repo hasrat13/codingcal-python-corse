62a0fcbc688b1bc5824033627e2738d56db4db34
hasrat13 <hasrat_rahil@students.edu.sg>
2025-06-09 18:11:35 +0800

    Add files via upload

---
+++ odd-even.py
@@ -0,0 +1,7 @@
+number=int(input("number needs to get checked : "))
+print("number to be checked :" , number )
+
+if number%2==0:
+    print("this is a even number ")
+else :
+    print("this is a odd number")
---
+++ smaller-great number.py
@@ -0,0 +1,7 @@
+num=-3
+if num <0:
+    print("this number is negative")
+
+num= 3 
+if num >0:
+    print("this number is postive ")
