a0df776bcdf5d37b628931e1c89f87e2c77ef480
hasrat13 <hasrat_rahil@students.edu.sg>
2025-06-12 15:58:53 +0800

    Add files via upload

---
+++ summer.py
@@ -0,0 +1,5 @@
+temperature=int(input("please enter the temperature : "))
+if(temperature < 20):
+    print("hey Rohan , its cold wear a jacket please")
+else:
+    print(" rohan its hot , please do not wear a jacket ")
